eef9aa4f9049de14e5b5a11c9b38bc48dbb62da2
TrungDT <thanhtrung.it25@gmail.com>
2019-05-25 11:30:58 +0700

    Song Control & styling

--- src/components/SongPlayer/component.js
+++ src/components/SongPlayer/component.js
@@ -3,7 +3,27 @@ import './SongPlayer.css';
 
 class SongPlayer extends Component {
   render() {
-    return <div className="song-player-container" />;
+    return (
+      <div className="song-player-container">
+        <div className="song-controls">
+          <div className="reverse-song">
+            <i className="fa fa-step-backward reverse" aria-hidden="true" />
+          </div>
+
+          <div className="play-btn">
+            <i className="fa fa-play-circle-o play-btn" aria-hidden="true" />
+          </div>
+
+          <div className="next-song">
+            <i className="fa fa-step-forward forward" aria-hidden="true" />
+          </div>
+        </div>
+
+        <div className="song-progress-container">
+          <div className="song-progress" />
+        </div>
+      </div>
+    );
   }
 }
 
