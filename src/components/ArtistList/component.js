d9f2f9c803eb2ee52b313df5ba5befd9cf1a6593
TrungDT <thanhtrung.it25@gmail.com>
2019-05-26 23:40:55 +0700

    add link to artist page

--- src/components/ArtistList/component.js
+++ src/components/ArtistList/component.js
@@ -5,21 +5,25 @@ class ArtistList extends Component {
   renderArtists() {
     return this.props.artists.map((artist, i) => {
       return (
-        <li className="artist-item" key={i}>
-          <div>
-            <div className="artist-image">
-              {
-                <img
-                  src={artist.images[0] ? artist.images[0].url : ''}
-                  alt="artist"
-                />
-              }
-            </div>
-            <div className="artist-details">
-              <p>{artist.name}</p>
-            </div>
-          </div>
-        </li>
+        artist.images[0] && (
+          <li className="artist-item" key={i}>
+            <a href={artist.external_urls.spotify}>
+              <div>
+                <div className="artist-image">
+                  {
+                    <img
+                      src={artist.images[0] ? artist.images[0].url : ''}
+                      alt="artist"
+                    />
+                  }
+                </div>
+                <div className="artist-details">
+                  <p>{artist.name}</p>
+                </div>
+              </div>
+            </a>
+          </li>
+        )
       );
     });
   }
