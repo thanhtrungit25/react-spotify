1feb679e7d97ac4c7653d994e1e723feda1df4ee
TrungDT <thanhtrung.it25@gmail.com>
2019-05-28 14:02:44 +0700

    Updates

---
+++ precache-manifest.a9c5adcc400d179ad72263af20ea04a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dd5425649f063ecefa8294b71ff2580",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f553e2b0b16526a6ed3d",
+    "url": "/static/css/main.ff09a4c2.chunk.css"
+  },
+  {
+    "revision": "4e748d4bd2f3e750ef42",
+    "url": "/static/js/2.5c9a241f.chunk.js"
+  },
+  {
+    "revision": "f553e2b0b16526a6ed3d",
+    "url": "/static/js/main.89ad85be.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
