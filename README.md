31f8357a648bb0556ffc29974dd49c440f2ffb68
TrungDT <thanhtrung.it25@gmail.com>
2019-05-28 15:33:05 +0700

    Update readme, change client id

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-React Spotify Clone
+# React-Spotify 🎼🎺🎸🎻🎤
+
+This is `React-Spotify`, a React front end client that communicates with the Spotify API.
+
+See it in action [here](https://thanhtrungit25.github.io/react-spotify/).
+
+![alt text](https://github.com/thanhtrungit25/react-spotify/blob/master/songs.png "Song")
+![alt text](https://github.com/thanhtrungit25/react-spotify/blob/master/browser.png "Browse")
+
+If you wish to run the code locally run the following.
+
+```
+yarn
+yarn start
+visit http://localhost:3000
--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ class App extends Component {
       // scope
       // user-read-private // user-read-email // playlist-read-private // user-library-read // user-library-modify // user-follow-read
       window.location.href =
-        'https://accounts.spotify.com/authorize?client_id=06f0c98364d948048c214c9e3ac2fc12&scope=user-read-private%20user-read-email%20user-top-read%20user-read-recently-played%20user-read-playback-state%20user-library-read%20user-library-modify%20user-follow-read%20playlist-read-private%20playlist-read-collaborative%20playlist-modify-public%20playlist-modify-private%20user-follow-read%20user-library-read%20user-library-modify&response_type=token&redirect_uri=http://localhost:3000/callback';
+        'https://accounts.spotify.com/authorize?client_id=de927175d3fc44e1b75422b9525b07d2&scope=user-read-private%20user-read-email%20user-top-read%20user-read-recently-played%20user-read-playback-state%20user-library-read%20user-library-modify%20user-follow-read%20playlist-read-private%20playlist-read-collaborative%20playlist-modify-public%20playlist-modify-private%20user-follow-read%20user-library-read%20user-library-modify&response_type=token&redirect_uri=http://localhost:3000/callback';
     } else {
       this.props.setToken(hashParams.access_token);
     }
