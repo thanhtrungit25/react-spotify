7f650cd360bf4e387a2f4a087ac3f094dddd16a7
TrungDT <thanhtrung.it25@gmail.com>
2019-05-28 14:49:11 +0700

    Updates

---
+++ precache-manifest.0140f14d45000658ef5fba6d65fa3460.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d97332ae2c2d99e6f58bb5574d4acbd1",
+    "url": "/react-spotify/index.html"
+  },
+  {
+    "revision": "35e00a74551b3ea06289",
+    "url": "/react-spotify/static/css/main.ff09a4c2.chunk.css"
+  },
+  {
+    "revision": "c84f8fb51d66c89980c3",
+    "url": "/react-spotify/static/js/2.41d27782.chunk.js"
+  },
+  {
+    "revision": "35e00a74551b3ea06289",
+    "url": "/react-spotify/static/js/main.e1d792ac.chunk.js"
+  },
+  {
+    "revision": "4cfa1170b08206d4c844",
+    "url": "/react-spotify/static/js/runtime~main.810ade09.js"
+  }
+]);
